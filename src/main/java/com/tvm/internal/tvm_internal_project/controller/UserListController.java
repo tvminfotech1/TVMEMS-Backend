21b89f89d44846d2514db9c52b4a44a4ebc63f0d
Jyotiranjan09 <jyotiranjan73234@gmail.com>
2025-10-30 10:12:47 +0530

    Userlist_changes

---
+++ src/main/java/com/tvm/internal/tvm_internal_project/controller/UserListController.java
@@ -0,0 +1,27 @@
+package com.tvm.internal.tvm_internal_project.controller;
+
+
+import com.tvm.internal.tvm_internal_project.model.User;
+import com.tvm.internal.tvm_internal_project.serviceImpl.UserListServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/userlist")
+public class UserListController {
+
+    @Autowired
+    UserListServiceImpl userListServiceImpl;
+
+    @GetMapping("/all")
+    public List<User> getAllUser(){
+        return userListServiceImpl.getAllUser();
+    }
+
+    @DeleteMapping("/delete/{employeeId}")
+    public void deleteUser(@PathVariable("employeeId") long employeeId){
+        userListServiceImpl.deleteUser(employeeId);
+    }
+}
